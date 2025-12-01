2abccd2e3220bdccac8af1f79e8994052efc7d44
marcvanheerden <11750833+marcvanheerden@users.noreply.github.com>
2025-12-01 07:56:10 +0000

    cleanup day01 rust

--- rust01/src/main.rs
+++ rust01/src/main.rs
@@ -1,43 +1,38 @@
 const POSITIONS: isize = 100;
 
 fn main() {
-    let input = std::fs::read_to_string("day01_large.txt").expect("Can't read input file");
+    let input = std::fs::read_to_string("input.txt").expect("Can't read input file");
 
     let mut dial_pos = 50isize;
-    let mut prev_dial_pos;
     let mut zeros = 0u32;
     let mut passes = 0u32;
 
     for line in input.lines() {
-        let mut chars = line.chars();
-        let dir = chars.next().unwrap();
-        let value = chars.collect::<String>().parse::<isize>().unwrap();
+        let (dir, value) = line.split_at(1);
+        let value: isize = value.parse().expect("Cant parse value");
 
-        prev_dial_pos = dial_pos;
-        if dir == 'R' {
-            dial_pos += value;
-        } else {
-            dial_pos -= value;
-        }
+        let full_rotations = value / POSITIONS;
+        let partial_rotation = value % POSITIONS;
 
-        if dial_pos < 0 {
-            let mut revs = dial_pos.abs() / POSITIONS;
-            passes += revs as u32;
-            if prev_dial_pos > 0 {
-                passes += 1;
+        match dir.chars().next() {
+            Some('R') => {
+                if (partial_rotation + dial_pos) >= POSITIONS {
+                    passes += 1;
+                }
+                dial_pos += value;
             }
-
-            if dial_pos.abs() % POSITIONS != 0 {
-                revs += 1;
+            Some('L') => {
+                if (partial_rotation >= dial_pos) & (dial_pos > 0) {
+                    passes += 1;
+                }
+                dial_pos -= value;
             }
-            dial_pos += revs * POSITIONS;
-        } else if dial_pos >= POSITIONS {
-            passes += (dial_pos / POSITIONS) as u32;
-            dial_pos %= POSITIONS;
-        } else if dial_pos == 0 {
-            passes += 1;
-        }
+            _ => panic!("Invalid input"),
+        };
 
+        passes += full_rotations as u32;
+
+        dial_pos = dial_pos.rem_euclid(POSITIONS);
         if dial_pos == 0 {
             zeros += 1;
         }
