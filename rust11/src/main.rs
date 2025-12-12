5367233f23ee7947c5db0b564bd3f4942d5de17e
marcvanheerden <11750833+marcvanheerden@users.noreply.github.com>
2025-12-13 00:36:29 +0200

    cleaner day 11

--- rust11/src/main.rs
+++ rust11/src/main.rs
@@ -1,65 +1,81 @@
-use std::collections::{HashMap, VecDeque};
+use std::collections::HashMap;
 
 fn paths(start: &str, edges: &HashMap<&str, Vec<&str>>) -> usize {
-    let mut queue = VecDeque::new();
-    queue.push_back(start);
+    let mut queue = Vec::new();
+    queue.push(start);
 
     let mut count = 0;
     loop {
         let mut next_queue = Vec::new();
-        while !queue.is_empty() {
-            let current = queue.pop_front().unwrap();
-
+        while let Some(current) = queue.pop() {
             for edge in edges.get(current).unwrap() {
                 if *edge == "out" {
                     count += 1;
                 } else {
-                    next_queue.push(edge);
+                    next_queue.push(*edge);
                 }
             }
         }
         if next_queue.is_empty() {
             break;
         }
-        queue = next_queue.into_iter().cloned().collect();
+        queue = next_queue;
     }
 
     count
 }
 
-fn paths2(start: &str, edges: &HashMap<&str, Vec<&str>>) -> usize {
+#[derive(Debug, Clone)]
+struct SplitCount {
+    visited_neither: u64,
+    visited_dac_only: u64,
+    visited_fft_only: u64,
+    visited_both: u64,
+}
+
+fn paths2(start: &str, edges: &HashMap<&str, Vec<&str>>) -> u64 {
     let mut queue = HashMap::new();
     // counts: visited neither, visited fft only, visited dac only, visited both
-    queue.insert(start, (1, 0, 0, 0));
+    queue.insert(
+        start,
+        SplitCount {
+            visited_neither: 1,
+            visited_dac_only: 0,
+            visited_fft_only: 0,
+            visited_both: 0,
+        },
+    );
 
     let mut count = 0;
     loop {
         let mut next_queue = HashMap::new();
         for (current, run_count) in queue.into_iter() {
-            if run_count.0 + run_count.1 + run_count.2 + run_count.3 == 0 {
+            if current == "out" {
                 continue;
             }
             for edge in edges.get(current).unwrap() {
-                let ent = next_queue.entry(*edge).or_insert((0, 0, 0, 0));
+                let ent = next_queue.entry(*edge).or_insert(SplitCount {
+                    visited_neither: 0,
+                    visited_dac_only: 0,
+                    visited_fft_only: 0,
+                    visited_both: 0,
+                });
                 match *edge {
-                    "out" => count += run_count.3,
+                    "out" => count += run_count.visited_both,
                     "fft" => {
-                        ent.0 += 0;
-                        ent.1 += run_count.0 + run_count.1;
-                        ent.2 += 0;
-                        ent.3 += run_count.2 + run_count.3;
+                        ent.visited_fft_only +=
+                            run_count.visited_fft_only + run_count.visited_neither;
+                        ent.visited_both += run_count.visited_both + run_count.visited_dac_only;
                     }
                     "dac" => {
-                        ent.0 += 0;
-                        ent.1 += 0;
-                        ent.2 += run_count.0 + run_count.2;
-                        ent.3 += run_count.1 + run_count.3;
+                        ent.visited_dac_only += run_count.visited_neither;
+                        ent.visited_both += run_count.visited_both + run_count.visited_fft_only;
                     }
                     _ => {
-                        ent.0 += run_count.0;
-                        ent.1 += run_count.1;
-                        ent.2 += run_count.2;
-                        ent.3 += run_count.3;
+                        ent.visited_neither += run_count.visited_neither;
+                        ent.visited_dac_only += run_count.visited_dac_only;
+                        ent.visited_fft_only += run_count.visited_fft_only;
+                        ent.visited_both += run_count.visited_both;
                     }
                 }
             }
